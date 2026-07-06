0512d47f517b55b9d87c14bc32174d7e02af5f0d
josephhashakimana4-code <josephhashakimana4@gmail.com>
2026-07-06 16:50:08 +0200

    Refactor role middleware to check single required role

--- middlewares/role.js
+++ middlewares/role.js
@@ -1,8 +1,13 @@
-module.exports = function role(...allowedRoles) {
+module.exports = function role(requiredRole) {
   return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Forbidden: insufficient permissions" });
+    if (!req.user) {
+      return res.status(401).json({ message: "SESSION_EXPIRED" });
     }
+
+    if (req.user.role !== requiredRole) {
+      return res.status(403).json({ message: "ACCESS_DENIED" });
+    }
+
     next();
   };
 };
